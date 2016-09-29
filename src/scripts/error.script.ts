3df2a68802aa5fa14f83ef915085e5cc40c4a228
David Wood <david.p.wood@gmail.com>
2016-09-29 17:34:51 +0100

    Print user error info only if response is set

--- src/scripts/error.script.ts
+++ src/scripts/error.script.ts
@@ -4,17 +4,24 @@ import Config from '../config';
 export default (robot: Robot) => {
 
   robot.error((err, response) => {
-    const user = response.envelope.user;
-    const fallback = `Error responding to message from @${user.name}
-Command: ${response.message.text}
-Error: ${err.message}`;
-    const text = `Error responding to message from @${user.name}
-\`\`\`
-Command: ${response.message.text}
-\`\`\`
-\`\`\`
+    let fallback = `Error: ${err.message}`;
+    let text = `\`\`\`
 Error: ${err.stack}
 \`\`\``;
+
+    if (response) {
+      response.reply('Uhh, sorry, I just experienced an error :goberserk:');
+
+      const user = response.envelope.user;
+      fallback = `Error responding to message from @${user.name}
+Command: ${response.message.text}
+${fallback}`;
+      text = `Error responding to message from @${user.name}
+\`\`\`
+Command: ${response.message.text}
+\`\`\`
+${text}`;
+  }
 
     robot.send({ room: Config.HACKBOT_ERROR_CHANNEL.value }, {
       attachments: [{
@@ -25,9 +32,5 @@ Error: ${err.stack}
         mrkdwn_in: ['text'],
       }],
     });
-
-    if (response) {
-      response.reply('Uhh, sorry, I just experienced an error :goberserk:');
-    }
   });
 };
--- src/test/error.test.ts
+++ src/test/error.test.ts
@@ -76,10 +76,66 @@ Error: ${error.stack}
       expect(loggerErrorStub).to.have.been.calledWith(error.stack);
     });
 
-    it('should welcome the user to the team', () => {
+    it('should report the error to the user', () => {
       expect(room.messages).to.eql([
         ['hubot', expectedMessage],
         ['hubot', `@${userName} Uhh, sorry, I just experienced an error :goberserk:`],
+      ]);
+    });
+
+    it('should print the message error in the slack error channel', () => {
+      expect(sendSpy).to.have.been.calledWith({ room: errorChannel });
+    });
+  });
+
+  describe('catches an error with Slack adapter not caused by a user', () => {
+
+    before(setUp);
+    after(tearDown);
+
+    let userName: string;
+    let error: Error;
+    let errorChannel: string;
+    let expectedMessage: ICustomMessageData;
+    let loggerErrorStub: sinon.SinonStub;
+    let sendSpy: sinon.SinonSpy;
+
+    before(() => {
+      userName = 'someUser';
+      room.user.name = userName;
+      error = new Error('catches an error with Slack adapter');
+      errorChannel = '#my_error_channel';
+
+      Config.HACKBOT_ERROR_CHANNEL.value = errorChannel;
+
+      const fallback = `Error: ${error.message}`;
+      const text = `\`\`\`
+Error: ${error.stack}
+\`\`\``;
+
+      expectedMessage = {
+        attachments: [{
+          fallback: fallback,
+          color: 'warning',
+          title: `Hackbot error caught`,
+          text: text,
+          mrkdwn_in: ['text'],
+        }],
+      };
+
+      loggerErrorStub = sinon.stub(robot.logger, 'error');
+      sendSpy = sinon.spy(room.robot, 'send');
+
+      room.robot.emit('error', error);
+    });
+
+    it('should log the error', () => {
+      expect(loggerErrorStub).to.have.been.calledWith(error.stack);
+    });
+
+    it('should not report the error to a user', () => {
+      expect(room.messages).to.eql([
+        ['hubot', expectedMessage],
       ]);
     });
 
