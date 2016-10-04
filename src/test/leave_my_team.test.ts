62e339e08875b432a64d4ae68eacdfa43434e5d3
Joshua Hawxwell <m@hawx.me>
2016-10-04 19:24:00 +0100

    Delete team when leaving own team and only member left

--- src/test/leave_my_team.test.ts
+++ src/test/leave_my_team.test.ts
@@ -78,6 +78,69 @@ describe('@hubot leave my team', () => {
     });
   });
 
+  describe('when in a team and only member of the team', () => {
+
+    before(setUp);
+    after(tearDown);
+
+    let userName: string;
+    let userEmail: string;
+    let existingTeamId: string;
+    let existingTeamName: string;
+    let getUserStub: sinon.SinonStub;
+    let removeTeamMemberStub: sinon.SinonStub;
+    let removeTeamStub: sinon.SinonStub;
+
+    before(() => {
+      userName = 'micah';
+      userEmail = 'micah.micah~micah';
+      existingTeamId = 'ocean-mongrels';
+      existingTeamName = 'Ocean Mongrels';
+
+      getUserStub = sinon.stub(robot.client, 'getUser').returns(Promise.resolve({
+        ok: true,
+        user: {
+          team: {
+            id: existingTeamId,
+            name: existingTeamName,
+            members: [{
+              name: 'a',
+            }],
+          },
+        },
+      }));
+
+      removeTeamMemberStub = sinon.stub(robot.client, 'removeTeamMember').returns(Promise.resolve({ ok: true }));
+
+      removeTeamStub = sinon.stub(robot.client, 'removeTeam').returns(Promise.resolve({ ok: true }));
+
+      sinon.stub(dataStore, 'getUserById')
+        .withArgs(userName)
+        .returns({ id: userName, profile: { email: userEmail } } as User);
+
+      return room.user.say(userName, '@hubot leave my team');
+    });
+
+    it('should get the user', () => {
+      expect(getUserStub).to.have.been.calledWith(userName);
+    });
+
+    it('should update the team, excluding the current user in the member list', () => {
+      expect(removeTeamMemberStub).to.have.been.calledWith(existingTeamId, userName, userEmail);
+    });
+
+    it('should delete the team', () => {
+      expect(removeTeamStub).to.have.been.calledWith(existingTeamId);
+    });
+
+    it('should tell the user that they have left the team and that the team has been deleted', () => {
+      expect(room.messages).to.eql([
+        [userName, '@hubot leave my team'],
+        ['hubot', `@${userName} OK, you've been removed from team "${existingTeamName}" and the team has been deleted.`],
+      ]);
+    });
+  });
+
   describe('when not a registered attendee', () => {
 
     before(setUp);
